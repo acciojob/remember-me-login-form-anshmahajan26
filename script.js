b9e00d065d7f756f1082f4ed255404c17f960eaa
Ansh Mahajan <anshmahajan678@gmail.com>
2026-02-01 12:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 let user = document.getElementById("username");
 let pass = document.getElementById("password");
 let check = document.getElementById("checkbox");
-let btn = document.getElementById("submit");
+let form = document.getElementById("form");
 let existing = document.getElementById("existing");
 
-// show existing user button if data exists
-if (localStorage.getItem("user")) {
+// show existing user if data present
+if (localStorage.getItem("username") && localStorage.getItem("password")) {
   existing.style.display = "block";
 }
 
-btn.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   alert(`Logged in as ${user.value}`);
 
   if (check.checked) {
-    localStorage.setItem("user", user.value);
-    localStorage.setItem("pass", pass.value);
+    localStorage.setItem("username", user.value);
+    localStorage.setItem("password", pass.value);
     existing.style.display = "block";
   } else {
-    localStorage.removeItem("user");
-    localStorage.removeItem("pass");
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
     existing.style.display = "none";
   }
 });
 
 existing.addEventListener("click", () => {
-  alert(`Logged in as ${localStorage.getItem("user")}`);
+  alert(`Logged in as ${localStorage.getItem("username")}`);
 });
